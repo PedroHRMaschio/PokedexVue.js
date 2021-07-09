787e827b96b5c595614d13c7b21059b5f07e3fe1
PedroHRMaschio <pedrohenriquermaschio@gmail.com>
2021-07-09 17:00:47 -0300

    inicio do site

--- pokedex/src/App.vue
+++ pokedex/src/App.vue
@@ -1,14 +1,30 @@
 <template>
   <div id="app">
-    
+    <div v-for="(poke,index) in pokemons" :key="index">
+      <pokemon :name="poke.name" :url="poke.url" :num="index+1"/>
+    </div>
   </div>
 </template>
 
 <script>
-//import axios from "axios";
+import axios from "axios";
+import Pokemon from "./components/Pokemon.vue";
 export default {
-  name: 'App'
-  
+  name: 'App',
+  data(){
+    return {
+      pokemons: []
+    }
+  },
+  created: function(){
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0").then(res => {
+      console.log("Lista de pokemons adquirida")
+      this.pokemons = res.data.results;
+    });
+  },
+  components: {
+    Pokemon
+  }
 }
 </script>
 
